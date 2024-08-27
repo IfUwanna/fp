b58bbd3e9230e9fb75ca21821bec3774557f3142
Jihun Park <jihun.park@navercorp.com>
2024-08-27 13:03:58 +0900

    chapter 3.3-4

--- src/test/kotlin/chapter3/exercises/ex3/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex3/listing.kt
@@ -1,31 +1,32 @@
 package chapter3.exercises.ex3
 
+import chapter3.Cons
 import chapter3.List
 import chapter3.Nil
-import io.kotest.matchers.shouldBe
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
 
-
-// tag::init[]
+// 3.3 tail을 더 일반화 해서 Drop 함수 작성, 리스트 맨 앞부터 n개의 원소 제거
 fun <A> drop(l: List<A>, n: Int): List<A> =
+    when {
+        n == 0 -> l
+        l is Cons -> drop(l.tail, n - 1)
+        else -> throw IllegalStateException("")
+    }
 
-    TODO()
-// end::init[]
-
-//TODO: Enable tests by removing `!` prefix
 class Exercise3 : WordSpec({
     "list drop" should {
-        "!drop a given number of elements within capacity" {
+        "drop a given number of elements within capacity" {
             drop(List.of(1, 2, 3, 4, 5), 3) shouldBe
-                List.of(4, 5)
+                    List.of(4, 5)
         }
 
-        "!drop a given number of elements up to capacity" {
+        "drop a given number of elements up to capacity" {
             drop(List.of(1, 2, 3, 4, 5), 5) shouldBe Nil
         }
 
-        """!throw an illegal state exception when dropped elements
+        """throw an illegal state exception when dropped elements
             exceed capacity""" {
             shouldThrow<IllegalStateException> {
                 drop(List.of(1, 2, 3, 4, 5), 6)
--- src/test/kotlin/chapter3/exercises/ex4/listing.kt
+++ src/test/kotlin/chapter3/exercises/ex4/listing.kt
@@ -1,40 +1,44 @@
 package chapter3.exercises.ex4
 
+import chapter3.Cons
 import chapter3.List
 import chapter3.Nil
+import chapter3.exercises.ex3.drop
 import io.kotest.matchers.shouldBe
 import io.kotest.core.spec.style.WordSpec
 
 
-// tag::init[]
+// List 맨앞에서부터 주어진 술어를 만족하는 연속적인 원소를 삭제 (만족하는 접두사를 List에서 제거)
 fun <A> dropWhile(l: List<A>, f: (A) -> Boolean): List<A> =
+    when {
+        l is Cons -> if(f(l.head)) dropWhile(l.tail, f) else l
+        else -> l
+    }
 
-    TODO()
-// end::init[]
 
-//TODO: Enable tests by removing `!` prefix
+
 class Exercise4 : WordSpec({
 
     "list dropWhile" should {
-        "!drop elements until predicate is no longer satisfied" {
+        "drop elements until predicate is no longer satisfied" {
             dropWhile(
                 List.of(1, 2, 3, 4, 5)
             ) { it < 4 } shouldBe List.of(4, 5)
         }
 
-        "!drop no elements if predicate never satisfied" {
+        "drop no elements if predicate never satisfied" {
             dropWhile(
                 List.of(1, 2, 3, 4, 5)
             ) { it == 100 } shouldBe List.of(1, 2, 3, 4, 5)
         }
 
-        "!drop all elements if predicate always satisfied" {
+        "drop all elements if predicate always satisfied" {
             dropWhile(
                 List.of(1, 2, 3, 4, 5)
             ) { it < 100 } shouldBe List.of()
         }
 
-        "!return Nil if input is empty" {
+        "return Nil if input is empty" {
             dropWhile(List.empty<Int>()) { it < 100 } shouldBe Nil
         }
     }
